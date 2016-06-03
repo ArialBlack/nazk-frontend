68a798b219b7864e33991e86a16a106d238d320d
ArialBlack <andreyturik@gmail.com>
2016-06-03 22:26:42 +0300

    init

--- sites/all/themes/nazk_ui/templates/page--basicpage.tpl.php
+++ sites/all/themes/nazk_ui/templates/page--basicpage.tpl.php
@@ -74,62 +74,91 @@
  */
  
  //$p = field_view_field('node', $node, 'field_main_pic', array('label'=>'hidden'));
- //print render($p); 
+ //print render($p);
+ 
+    $body = field_view_field('node', $node, 'body', array(
+        'label'=>'hidden',
+        'type' => 'text_summary_or_trimmed', 
+        'settings'=>array('trim_length' => 200),
+    ));
     
-    $img_url = $node->field_main_pic['und'][0]['uri'];
-    $style = "cover";
+    if ($node->field_main_pic) {
+        $img_url = $node->field_main_pic['und'][0]['uri'];
+    } else $img_url = "";
 ?>
 
 <section class="node-news-intro">
-    <div class="node-cover" style="background-image: url('<?php print image_style_url($style, $img_url) ?>')">
-    </div>
+    <div class="node-cover" 
+    <?php if (strlen($img_url) > 0): ?>
+         style="background-image: url('<?php print image_style_url("cover", $img_url) ?>')"
+    <?php endif; ?>
+    ></div>
+        <div class="pre-header">
+            <div class="container">
+                <?php if ($logo): ?>
+                    <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+                      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+                    </a>
+                <?php endif; ?>
+        
+                <?php if (!empty($site_name)): ?>
+                    <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+                <?php endif; ?>
+              
+                <div class="social-links pull-right">
+                    <a href="#"><i class="icon ion-social-facebook"></i></a>
+                    <a href="#"><i class="icon ion-social-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+        
+        <header id="navbar" role="banner" class="navbar navbar-default">
+            <div class="<?php print $container_class; ?>">
+                <div class="navbar-header">
+            
+                  <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                      <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                  <?php endif; ?>
+                </div>
+            
+                <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
+                  <div class="navbar-collapse collapse">
+                    <nav role="navigation">
+                      <?php if (!empty($primary_nav)): ?>
+                        <?php print render($primary_nav); ?>
+                      <?php endif; ?>
+                      <?php if (!empty($secondary_nav)): ?>
+                        <?php print render($secondary_nav); ?>
+                      <?php endif; ?>
+                      <?php if (!empty($page['navigation'])): ?>
+                        <?php print render($page['navigation']); ?>
+                      <?php endif; ?>
+                    </nav>
+                  </div>
+                <?php endif; ?>
+              </div>
+        </header>
+    
+        <div class="container">
+            <?php print render($title_prefix); ?>
+            <?php if (!empty($title)): ?>
+                <h1 class="page-header"><?php print $title; ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            
+            <div class="submitted">
+                <?php print format_date($node->created, 'custom','d.F.Y'); ?>
+            </div>
+            
+            <b><?php print render($body); ?></b>
+        </div> 
 </section>
 
-<div class="pre-header">
-      <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-        </a>
-      <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-      
-      <i class="icon ion-social-facebook"></i>
-</div>
-
-<header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
-  <div class="<?php print $container_class; ?>">
-    <div class="navbar-header">
-
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-      <?php endif; ?>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse">
-        <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-      </div>
-    <?php endif; ?>
-  </div>
-</header>
 
 <div class="main-container <?php print $container_class; ?>">
 
@@ -155,11 +184,7 @@
       <?php endif; ?>
       <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
       <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if (!empty($title)): ?>
-        <h1 class="page-header"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
+      
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
         <?php print render($tabs); ?>
