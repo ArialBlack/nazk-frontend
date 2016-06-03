725284f970b0505d49568ba18a6c65d72851f7a7
ArialBlack <andreyturik@gmail.com>
2016-06-03 18:09:31 +0300

    add page template

--- sites/all/themes/nazk_ui/template.php
+++ sites/all/themes/nazk_ui/template.php
@@ -3,3 +3,22 @@
  * @file
  * The primary PHP file for this theme.
  */
+
+function nazk_ui_preprocess_page(&$vars) {
+    // - page--example.tpl.php  
+
+    if (isset($vars['node'])) {
+        $vars['theme_hook_suggestion'] = 'page__'.$vars['node']->type; //
+    }
+    
+    global $language;
+    $lang = $language->language;
+
+    if ($lang == 'en') {
+        $vars['logo'] = '/sites/default/files/NAZK_logo_en.png';
+    }
+    
+    if ($lang == 'uk') {
+        $vars['logo'] = '/sites/default/files/NAZK_logo.png';
+    }
+}
